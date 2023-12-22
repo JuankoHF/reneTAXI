44372448ab1dd54158d23fdfce6c4af7fbe29ef1
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-12-22 17:56:52 +0100

    lista para movil small

---
+++ js/class-scrollReveal.js
@@ -0,0 +1,41 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('.go_contact', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px'
+    });
+
+    sr.reveal('.linck', {
+        duration: 3000,
+        origin: 'left',
+        distance: '60px'
+    });
+    // sr.reveal('.header_right', {
+    //     duration: 4000,
+    // });
+
+    // sr.reveal('.get_budget', {
+    //     duration: 3000,
+    //     origin: 'top',
+    //     distance: '-100px'
+    // });
+
+    // sr.reveal('.tope_etymology', {
+    //     duration: 2000,
+    //     delay: 1000,
+    //     origin: 'left',
+    //     distance: '-100px'
+    // });
+
+    // sr.reveal('.content_tope_contact', {
+    //     duration: 3000,
+    //     origin: 'left',
+    //     distance: '300px'
+    // });
+
+    // sr.reveal('.tope_contact_left', {
+    //     duration: 2500,
+    //     origin: 'bottom',
+    //     distance: '200px'
+    // });
